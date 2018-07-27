e9cc4b1c44cc3a395244ce288328da975e7b719b
Peaceman <muhammedcy@gmail.com>
2018-07-27 16:25:22 +0300

    Section 9

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,6 +8,24 @@
 
 import Foundation
 
+
+typealias CompletionHandler = (_ Succes: Bool) ->()
+
+
+// URL Constans
+
+let BASE_URL = "https://my-new-chat-chat-app.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
+//Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATEACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+//User Deafaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
